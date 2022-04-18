b7b9515898c38f7e611d18394aa42bdcb6afe6a7
Michał Flak <mflak@monadical.com>
2022-04-18 04:59:53 +0200

    tests

---
+++ tests/fizzbuzz-recursive.scm
@@ -0,0 +1,11 @@
+(define (fizzbuzz x y)
+  (display
+    (cond (( = (modulo x 15) 0 ) "FizzBuzz")
+          (( = (modulo x 3) 0 ) "Fizz")
+          (( = (modulo x 5) 0 ) "Buzz")
+          (else x)))
+  (newline)
+  
+  (if (< x y) (fizzbuzz (+ x 1) y)))
+ 
+(fizzbuzz 1 100)
